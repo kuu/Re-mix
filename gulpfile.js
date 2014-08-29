3471f26d2bbdc5e0269c8c1fbc105c8f7faab7bd
Kuu Miyazaki <miyazaqui@gmail.com>
2014-08-30 02:48:28 +0900

    Making gulpfile work.

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,157 @@
 'use strict';
 
 var gulp = require('gulp');
+var glob = require('glob');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
 
 // load plugins
 var $ = require('gulp-load-plugins')();
 
-gulp.task('styles', function () {
-    return gulp.src('app/styles/main.scss')
-        .pipe($.rubySass({
-            style: 'expanded',
-            precision: 10
-        }))
-        .pipe($.autoprefixer('last 1 version'))
-        .pipe(gulp.dest('.tmp/styles'))
-        .pipe($.size());
-});
+var stylesheetsDir = './assets/stylesheets';
 
-gulp.task('handlebars', function () {
-    return gulp.src(['app/templates/**/*.hbs', '!app/templates/**/__*.hbs'])
-        .pipe($.handlebars())
-        .pipe($.defineModule('commonjs'))
+// error handling
+var handleError = function (err) {
+    $.util.log(err.toString());
+    this.emit('end');
+};
+
+var plumber = function () {
+    return $.plumber({ errorHandler : handleError });
+};
+
+//------------------------------------------------
+// HANDLEBARS
+//------------------------------------------------
+gulp.task('handlebars:compile', function () {
+
+    return gulp.src('./app/templates/**/[!__]*.hbs')
+        .pipe(plumber())
+        .pipe($.handlebars({ wrapped : true }))
+        .pipe($.wrap('templates["<%= file.relative.replace(/\\\\/g, "/").replace(/.js$/, "") %>"] = <%= file.contents %>;\n'))
         .pipe($.concat('compiledTemplates.js'))
+        .pipe($.wrap('module.exports = function(Handlebars){\ntemplates = {};\n<%= contents %>\nreturn templates \n};'))
         .pipe(gulp.dest('app/templates/'));
 });
 
-gulp.task('scripts', function () {
-    return gulp.src('app/scripts/**/*.js')
-        .pipe($.jshint())
-        .pipe($.jshint.reporter(require('jshint-stylish')))
-        .pipe($.size());
+gulp.task('handlebars:watch', function () {
+    return gulp.watch('./app/templates/**/*.hbs', [ 'handlebars:compile' ]);
 });
+
+//------------------------------------------------
+// STYLUS
+//------------------------------------------------
+
+gulp.task('stylus:compile', function () {
+
+    var opts = { set : [ 'include css' ] };
+
+    return gulp.src(stylesheetsDir + '/index.styl')
+        .pipe(plumber())
+        .pipe($.stylus(opts))
+        .pipe($.concat('styles.css'))
+        .pipe(gulp.dest('./public'));
+});
+
+gulp.task('stylus:watch', function () {
+    gulp.watch(stylesheetsDir + '/index.styl', [ 'stylus:compile' ]);
+});
+
+//------------------------------------------------
+// BROWSERIFY
+//------------------------------------------------
+
+// get rendr shared & client files list
+var rendrClientFiles = glob.sync('rendr/{client,shared}/**/*.js', { cwd : './node_modules/' });
+
+var rendrModules = rendrClientFiles.map(function (file) {
+    return file.replace('.js', '')
+});
+
+var getBundler = function (globs) {
+
+    var bundler, files;
+
+    bundler = browserify({
+        fullPaths : false,
+        entries   : []
+    });
+
+    globs.forEach(function (pattern) {
+        files = glob.sync(pattern, { cwd : './' });
+        files.forEach(function (file) {
+            // it's nesessary for some app modules (e.g. 'app/app')
+            // to be exposed otherwise rendr couldn't require them
+            moduleName = file.replace(/.js$/, '')
+            bundler.require('./' + file, { expose: moduleName});
+        });
+    });
+
+    rendrModules.forEach(function (moduleName) {
+        bundler.require(moduleName);
+    });
+
+    bundler.require('rendr-handlebars');
+    bundler.require('./assets/vendor/jquery-1.9.1.min.js', { expose : 'jquery' });
+
+    return bundler;
+};
+
+gulp.task('browserify:app', [ 'handlebars:compile' ], function () {
+
+    var bundler = getBundler([ 'app/**/*.js' ]),
+        options = { insertGlobals : false, debug : true };
+
+    return bundler.bundle(options)
+        .on('error', handleError)
+        .pipe(plumber())
+        .pipe(source('mergedAssets.js'))
+        .pipe(gulp.dest('./public'));
+});
+
+gulp.task('browserify:test', function () {
+
+    var bundler = getBundler([ 'test/app/**/*.js', 'app/**/*.js', 'test/helper.js' ]),
+        options = { insertGlobals : false, debug : true };
+
+    return bundler.bundle(options)
+        .on('error', handleError)
+        .pipe(plumber())
+        .pipe(source('testBundle.js'))
+        .pipe(gulp.dest('./public'));
+});
+
+gulp.task('browserify:watch', function () {
+    gulp.watch('./app/**/*.js', [ 'browserify:app' ])
+});
+
+//------------------------------------------------
+// BUILD
+//------------------------------------------------
 
 gulp.task('clean', function () {
     return gulp.src(['.tmp', 'dist'], { read: false }).pipe($.clean());
 });
 
-gulp.task('build', ['html', 'images', 'fonts', 'extras']);
+gulp.task('build', [ 'handlebars:compile', 'browserify:app',   'browserify:test', 'stylus:compile' ]);
 
-gulp.task('default', ['clean'], function () {
-    gulp.start('build');
+gulp.task('default', [ 'clean', 'build' ]);
+
+//------------------------------------------------
+// RUN
+//------------------------------------------------
+
+gulp.task('runNode', function () {
+    $.nodemon({
+        script : 'index.js',
+        ext    : 'js',
+        ignore : [ 'public/', 'gulpfile.js' ]
+    });
 });
+
+
+//gulp.task('watch',   [ 'handlebars:watch',   'browserify:watch', 'stylus:watch' ]);
+//gulp.task('server',  [ 'compile', 'runNode', 'watch' ]);
 
 gulp.task('connect', function () {
     var connect = require('connect');
